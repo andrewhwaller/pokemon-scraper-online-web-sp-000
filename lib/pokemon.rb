853ea9bb09d8fe666344e4ec3f90fc298d877291
Andrew Waller <andrewhwaller@gmail.com>
2019-04-14 14:42:44 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,7 +9,7 @@ class Pokemon
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
 
-  def find(id)
+  def self.find(id)
     search_results = db.execute("SELECT * FROM pokemon WHERE id=?", id)
     Pokemon.new(id: search_results[0], name:search_results[1], type: search_results[2], db: db)
   end
